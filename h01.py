d9b98e1311d30a8c09e103131640bdc445395e79
remitre-remi <remi.treier@gmail.com>
2025-10-09 14:50:22 +0300

    Add files via upload

---
+++ h01.py
@@ -0,0 +1,43 @@
+print("Tere maailm!")
+import turtle
+
+# #liigu kuhugi
+# turtle.penup()
+# turtle.goto(-400,200)
+# turtle.pendown()
+# 
+# #kolmnurk
+# for i in range(3): #tsükkel 3x
+#     turtle.fd(200) # fd on forward
+#     turtle.lt(120) # lt on left
+# 
+# #liigu kuhugi
+# turtle.penup()
+# turtle.goto(0,0)
+# turtle.pendown()
+# 
+# #ruut
+# for i in range(4):
+#     turtle.fd(150)
+#     turtle.lt(90)
+# 
+# #liigu kuhugi
+# turtle.penup()
+# turtle.goto(200,0)
+# turtle.pendown()
+# 
+#R
+
+turtle.circle(50,180)
+turtle.fd(50)
+turtle.lt(90)
+turtle.fd(200)
+turtle.lt(90)
+turtle.fd(40)
+turtle.lt(90)
+turtle.fd(60)
+turtle.rt(145)
+turtle.fd(80)
+
+
+turtle.done()
