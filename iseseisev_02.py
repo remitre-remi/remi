7e02670965025cf0f6c1c2bf38650277ca02f791
remitre-remi <remi.treier@gmail.com>
2025-11-05 23:14:22 +0200

    valmis

---
+++ iseseisev_02.py
@@ -0,0 +1,21 @@
+import turtle
+
+
+
+for i in range(5):
+    turtle.rt(90)
+    turtle.fd(50)
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.lt(90)
+    turtle.fd(100)
+    turtle.lt(90)
+    turtle.fd(50)
+    turtle.lt(162)
+
+
+
+
+turtle.done()
